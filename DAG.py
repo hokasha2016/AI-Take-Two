042f43165e7036dd6f3269d9474e6045cd55f7be
aesen1 <esenalex1@gmail.com>
2021-02-22 14:00:23 -0500

    integrated cyclic checking

--- DAG.py
+++ DAG.py
@@ -1,68 +1,114 @@
 import sys as sys
 
-class AdjNode:
-    def __init__(self, data):
-        self.vertex = data
-        self.next = None
+from collections import defaultdict
+
+class NodeD():
+
+    def __init__(self, pos,bool):
+        self.nodeDic={
+            "name":pos,
+            "boolean":bool,
+            "next":0
+        }
+    def setNext(self, newpos):
+        self.nodeDic["next"]=newpos
+        print(self.nodeDic["next"])
+        print(self.nodeDic)
 
 
-# A class to represent a graph. A graph
-# is the list of the adjacency lists.
-# Size of the array will be the no. of the
-# vertices "V"
-class Graph:
+class Graph():
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = [None] * self.V
+        self.graph = [None]*self.V
 
-        # Function to add an edge in an undirected graph
-
-    def add_edge(self, src, dest):
-        # Adding the node to the source node
-        node = AdjNode(dest)
+    #def
+    def addEdge(self, src, dest, bool):
+        node = NodeD(dest,bool)
         node.next = self.graph[src]
         self.graph[src] = node
 
         # Adding the source node to the destination as
         # it is the undirected graph
-        node = AdjNode(src)
+        node = NodeD(src,bool)
         node.next = self.graph[dest]
         self.graph[dest] = node
+        node.setNext(dest)
 
-        # Function to print the graph
+    def isCyclicUtil(self, v, visited, recStack):
+
+        # Mark current node as visited and
+        # adds to recursion stack
+        visited[v] = True
+        recStack[v] = True
+
+        # Recur for all neighbours
+        # if any neighbour is visited and in
+        # recStack then graph is cyclic
+        hood = []
+        for node in self.graph:
+            if node.nodeDic["name"] == v:
+                hood.append(node.nodeDic["next"])
+
+        for neighbour in hood:
+            if visited[neighbour]==False:
+                if self.isCyclicUtil(neighbour, visited, recStack)==True:
+                    return True
+            elif recStack[neighbour]==True:
+                return True
+
+        # The node needs to be poped from
+        # recursion stack before function ends
+        recStack[v] = False
+        return False
+
+    # Returns true if graph is cyclic else false
+    def isCyclic(self):
+        visited = [False] * self.V
+        recStack = [False] * self.V
+        for node in range(self.V):
+            if visited[node]==False:
+                    if self.isCyclicUtil(node, visited, recStack)==True:
+                        return True
+        return False
 
     def print_graph(self):
         for i in range(self.V):
             print("Adjacency list of vertex {}\n head".format(i), end="")
             temp = self.graph[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
+            print(temp)
             print(" \n")
+
+
+                # Driver program to the above graph class
+
+        # number of nodes, nodes as Boolean variables, arcs, and probability tables on nodes
+        # (prior probability on source nodes, conditional probabilities on other nodes).
+
+def main(n):
+
+    g = Graph(n)
+    g.addEdge(0, 1, False)
+    g.addEdge(0, 4, False)
+    g.addEdge(1, 2, True)
+    g.addEdge(1, 3, False)
+    g.addEdge(1, 4, True)
+    g.addEdge(2, 3, False)
+    g.addEdge(3, 4, True)
+    g.addEdge(0, 2, False)
+    g.addEdge(2, 0, True)
+    if g.isCyclic() == 1:
+        print("Graph has a cycle")
+    else:
+        print("Graph has no cycle")
+
+    g.print_graph()
+
 
         # Driver program to the above graph class
 #number of nodes, nodes as Boolean variables, arcs, and probability tables on nodes
 # (prior probability on source nodes, conditional probabilities on other nodes).
 
-def main():
-
-    V = 5
-    graph = Graph(V)
-    graph.add_edge(0, 1)
-    graph.add_edge(0, 4)
-    graph.add_edge(1, 2)
-    graph.add_edge(1, 3)
-    graph.add_edge(1, 4)
-    graph.add_edge(2, 3)
-    graph.add_edge(3, 4)
-
-    graph.print_graph()
-
 
 if __name__ == "__main__":
-    print(sys.argv[0])
-    print(sys.argv[1])
-    print(sys.argv[2])
-    print(sys.argv[3])
-    print(sys.argv[4]) #ERROR-- NOT BEING PRINTED
-    main()
+    main(5)
+
